ca48a92f73c3dfee4e4403bfb5ff3333810707ca
Quoc-An Ho <quocanho29@gmail.com>
2016-08-09 23:00:32 +1000

    Completed strict modification of hash context in hashSinkStrict.hs

--- playground/http-simple/hashSinkStrict.hs
+++ playground/http-simple/hashSinkStrict.hs
@@ -4,7 +4,7 @@ import Crypto.Hash (hashInitWith, SHA256(..), Digest(..), Context(..), hashUpdat
 import Control.Monad.IO.Class (liftIO)
 import Data.Conduit (yield, await, Source, Conduit, Sink, ($$+-), ($=+))
 import Data.Conduit.Binary (sinkFile)
-import Network.HTTP.Conduit (parseRequest, tlsManagerSettings, newManager, http, responseBody)
+import Network.HTTP.Conduit (parseRequest, tlsManagerSettings, newManager, http, responseBody, responseHeaders)
 import Control.Monad.Trans.Resource (runResourceT, ResourceT(..))
 import Control.Monad.Trans.Class (lift)
 import qualified Data.ByteString.Char8 as ByteS (ByteString, pack, length) 
@@ -16,31 +16,37 @@ import qualified Data.ByteString.Char8 as ByteS (ByteString, pack, length)
 --Initialise a hashing context
 hContext = hashInitWith SHA256
 
-main = streamHash "http://releases.nixos.org/nixos/16.03/nixos-16.03.1171.9cb194c/nixos-graphical-16.03.1171.9cb194c-x86_64-linux.iso" 
+main = do
+  digest <- streamHash "http://mirror.internode.on.net/pub/test/10meg.test"
+  print digest
 
 --download a binary file and produce a resumableSource
 --streams at variable chunk sizes
-streamHash :: String -> IO ()
+streamHash :: String -> IO (String)
 streamHash url = do
   request <- parseRequest url 
   manager <- newManager tlsManagerSettings
   runResourceT $ do
     response <- http request manager 
-    responseBody response $$+- hashSink hContext
+    let head = responseHeaders response 
+    let first = head !! 1
+    liftIO $ print $ show $ fst first
+    liftIO $ print $ show $ snd first
+    digestString <- responseBody response $$+- hashSink hContext
+    liftIO $ return digestString
 
 --receive bytestrings from upstream, and update hashing context
 --when done, print the hash
-hashSink :: (Context SHA256) -> Sink ByteS.ByteString (ResourceT IO) ()
+hashSink :: (Context SHA256) -> Sink ByteS.ByteString (ResourceT IO) (String)
 hashSink !hc = do
   mbs <- await
   case mbs of
     Just bs -> do
-      liftIO $ print $ show $ ByteS.length bs
       let context = hashUpdate hc bs
       hashSink context
     Nothing -> do
       let digest = hashFinalize hc 
-      liftIO $ print $ show digest
+      return $ show digest
 
 --test stream some bytestrings as source to sink
 testSource :: Source IO ByteS.ByteString
