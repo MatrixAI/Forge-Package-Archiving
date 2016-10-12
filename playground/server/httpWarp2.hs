2324f20734d810da4bd19109ac0bc1a691d79b28
Quoc-An Ho <quocanho29@gmail.com>
2016-10-12 21:51:44 +1100

    Use a broadcast TBMChan. This requires a modified stm-chan that exports TBMChan data constructor

--- playground/server/httpWarp2.hs
+++ playground/server/httpWarp2.hs
@@ -4,24 +4,47 @@
 
 import Data.ByteString.Builder (byteString)
 import qualified Data.ByteString.Char8 as BS (unpack, pack, length, ByteString(..))
-import Data.Conduit (($$), unwrapResumable, yield, await, ($$+-), ($=+), Conduit, ResumableSource)
+
+import Data.Conduit (unwrapResumable, yield, await, Conduit, ResumableSource, ($$), (=$=))
 import Data.Conduit.Binary (sinkFile)
 import qualified Data.Conduit.List as CL
+
 import Data.Maybe (fromMaybe)
 import Data.Typeable
+
 import Network.HTTP.Conduit (parseRequest, newManager, Manager(..), tlsManagerSettings, http, responseBody, Response(..))
 import Network.Wai (Application(..), responseStream, responseLBS, queryString, requestMethod)
-import Network.Wai.Handler.Warp (run)
+import Network.Wai.Handler.Warp (run, runSettings, Settings(..), defaultSettings, exceptionResponseForDebug, setOnExceptionResponse, setOnException)
 import Network.HTTP.Types (status200, status404)
+
 import Control.DeepSeq (NFData(..), force)
-import Control.Exception (catch)
+import Control.Exception (catch, SomeException)
+
 import Control.Monad (when)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Resource (runResourceT, ResourceT(..))
+import Control.Monad.Trans.Class (lift)
+
 import Control.Parallel.Strategies (parMap, rpar, rseq, rdeepseq)
+
 import Crypto.Hash (hashInitWith, HashAlgorithm, Digest(..), Context(..), hashUpdate, hashFinalize, 
   SHA256(..), MD5(..))
 
+import Control.Concurrent (forkIO)
+import Control.Concurrent.STM
+import Control.Concurrent.STM.TVar (TVar(..), newTVar)
+import Control.Concurrent.STM.TChan (newBroadcastTChan, dupTChan)
+import Control.Concurrent.STM.TBMChan (TBMChan(..))
+import Control.Concurrent.STM.TMChan (dupTMChan, newBroadcastTMChan)
+import Control.Concurrent.Async (concurrently)
+import Control.Exception (bracket)
+import Data.Conduit.TMChan (sinkTBMChan, sourceTBMChan)
+import Data.Conduit.Async (($$&), (=$=&))
+
+import qualified Data.Text.IO as TIO
+import qualified Data.Text as T
+
+import System.IO (stdout, stderr)
 -- quick data class to pack instances of context and digest into
 data Contextable = forall h. (HashAlgorithm h) => Contextable (Context h) 
 instance NFData Contextable where rnf (Contextable ctx) = rnf ctx 
@@ -52,6 +75,18 @@ hashC !ctxlist = do
       -- TODO: instead of printing hash values, write hash info to multikey index along with file information
       liftIO $ print $ map (\(Digestable digest) -> show digest) $ digestable
 
+newBroadcastTBMChan :: Int -> STM (TBMChan a)
+newBroadcastTBMChan n = do
+    closed <- newTVar False
+    slots <- newTVar n
+    reads <- newTVar 0
+    chan  <- newBroadcastTChan
+    return (TBMChan closed slots reads chan)
+
+dupTBMChan :: TBMChan a -> STM (TBMChan a)
+dupTBMChan (TBMChan closed slots reads chan) = do
+    new_chan <- dupTChan chan
+    return (TBMChan closed slots reads new_chan)
 
 app :: Application
 app req respond = do
@@ -66,14 +101,34 @@ app req respond = do
                         downloadRequest <- liftIO $ parseRequest $ BS.unpack url
                         downloadManager <- liftIO $ newManager tlsManagerSettings        
                         package <- http downloadRequest downloadManager
-                        let packageSource = responseBody $ package
---                        let packageSource = (http downloadRequest downloadManager) >>= responseBody
-
+                        (packageSource, _) <- unwrapResumable $ responseBody $ package
                         let sinkToClient = CL.mapM_ (\bs -> liftIO $ write (byteString bs) >> flush)
-                        -- TODO: write the binary file to server first, then IPFS, concurrently as you are hashing and feeding the output from the source back to the client
+
+                        chan <- liftIO $ atomically $ newBroadcastTBMChan 16
+                        chanR1 <- liftIO $ atomically $ dupTBMChan chan
+                        chanR2 <- liftIO $ atomically $ dupTBMChan chan
+
+                        -- the underlying TBMChan closes when the sink closes
+                        sourceID <- liftIO . forkIO . runResourceT $ packageSource $$& sinkTBMChan chan True
+
+                        -- waiting  on two child sinks to complete
+                        (_,_) <- liftIO $ concurrently 
+                            (catch (runResourceT $ (sourceTBMChan chanR1) $$& sinkToClient)
+                                (\e -> print ("line 94:" ++ show (e :: SomeException)) >> return ())
+                            )
+                            (catch (runResourceT $ (sourceTBMChan chanR2) =$=& hashC contexts $$& sinkFile "assets/testfile")
+                                (\e -> print ("line 97:" ++ show (e :: SomeException)) >> return ())
+                                )
+
+                        return ()
+                        
                         -- TODO FURTHER: have the app handle routing, so that we have two modes, a query mode and a binary download mode, and permalinks
-                        packageSource $=+ hashC contexts $$+- sinkToClient
+                        --packageSource $=+ hashC contexts $$+- sinkToClient
+                        --
+testExceptions _ e = TIO.hPutStrLn stdout $ T.pack $ show e
 
 main :: IO ()
 main = do
-    run 8000 app
+    --let settings = setOnException testExceptions defaultSettings
+    --let settings = defaultSettings { settingsOnExceptionResponse = exceptionResponseForDebug }
+    runSettings defaultSettings app
