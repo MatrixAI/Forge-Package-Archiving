9ac363fbf679ec2b2dae9bd564acecda49c3e04a
Quoc-An Ho <quocanho29@gmail.com>
2016-12-15 19:31:51 +1100

    Hide .proto directory from git

--- playground/.proto/socatServe.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-socat -d -d -v TCP-LISTEN:80,crlf,reuseaddr,fork SYSTEM:"
-	echo HTTP/1.1 200 OK;
-       	echo Content-Type\: text/plain;
-	echo Content-Length\: 6000000;
-       	echo;
-       	curl 'http://mirror.internode.on.net/pub/test/5meg.test1'"
-	# sleep 3s && kill %1 && exit 1;
--- playground/server/resT.hs
+++
@@ -1,4 +0,0 @@
-import Control.Monad.Trans.Resource (runResourceT)
-
-main = do
-    runResourceT
